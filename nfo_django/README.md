08b1ec0ebf63d7242e5b26bf0aec7469bfc0b62e
Vamshi Namilikonda <vamshi.namilikonda@capgemini.com>
2024-03-18 18:47:19 +0530

    testing

--- nfo_django/README.md
+++ nfo_django/README.md
@@ -1,4 +1,16 @@
 
+read helm chart using python
+https://github.com/helm/helm/issues/12267
+
+python3 -m venv env
+source env/bin/activate  # On Windows use `env\Scripts\activate`
+
+pip install django
+pip install djangorestframework
+pip3 install pyhelm
+pip3 install kubernetes
+
+
 python3 manage.py migrations
 python3 manage.py migrate
 python3 manage.py runserver
--- nfo_django/apis/views.py
+++ nfo_django/apis/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+import pyhelm
+from kubernetes import client
 
 # Create your views here.
 # import viewsets
@@ -26,6 +28,23 @@ def api_home(request, *args, **kwargs):
 
 def process_helm_charts():
     print("Processing Helm charts")
+    # Load the Helm chart
+    chart = pyhelm.load_chart('./my-chart')
+
+    # Get the chart's values
+    values = chart.values
+
+    # Get the chart's templates
+    templates = chart.templates
+
+    # Render the chart's templates
+    rendered_templates = chart.render_templates(values)
+
+    # Get the chart's manifest
+    manifest = chart.manifest
+
+    # Deploy the chart to Kubernetes
+    client.CoreV1Api().create_namespaced_deployment(namespace='default', body=manifest)
 
 
 
